31f8874248c9fdd7a944720c7cba9d63f6425271
Wysper <wysperotaku@gmail.com>
2025-05-22 19:05:32 +0200

    Requesting top animes

--- src/main/java/api/MyAnimeListClient.java
+++ src/main/java/api/MyAnimeListClient.java
@@ -1,5 +1,57 @@
 package api;
 
-public class MyAnimeListClient {
+import com.google.gson.Gson;
+import model.classes.Anime;
+import model.classes.TokenInfo;
+import model.dao.mysql.MySQLAnimeDAO;
+import model.response.AnimeSearchResponse;
+import model.response.AnimeWrapper;
+import service.OAuthService;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAnimeListClient {
+    private HttpClient client;
+    private TokenInfo token;
+
+    public MyAnimeListClient() {
+        this.client = HttpClient.newHttpClient();
+        this.token = OAuthService.cargarToken();
+    }
+
+    public List<Anime> getTopAnimes () throws IOException, InterruptedException {
+        String endpoint = "https://api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=100";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endpoint))
+                .header("Authorization", "Bearer " + token.getAccess_token())
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 401) {
+            OAuthService.actualizarTokenFile(token);
+            token = OAuthService.cargarToken();
+            return getTopAnimes();
+        }
+
+        if (response.statusCode() != 200) {
+            throw new IOException("Error en la petición: " + response.statusCode());
+        }
+
+        Gson gson = new Gson();
+        AnimeSearchResponse animeResponse = gson.fromJson(response.body(), AnimeSearchResponse.class);
+        List<Anime> animes = new ArrayList<>();
+        for (AnimeWrapper anime : animeResponse.getData()) {
+            animes.add(anime.node);
+        }
+
+        return animes;
+    }
 }
--- src/main/java/model/response/AnimeSearchResponse.java
+++ src/main/java/model/response/AnimeSearchResponse.java
@@ -5,13 +5,13 @@ import model.classes.Anime;
 import java.util.List;
 
 public class AnimeSearchResponse {
-    private List<Anime> data;
+    private List<AnimeWrapper> data;
 
     // Constructor sin parametros para que GSON me devuelva el objeto que necesito
     //Usando reflexion GSON va a ir creando la lista 1 a 1
     public AnimeSearchResponse() {}
 
-    public List<Anime> getData() {
+    public List<AnimeWrapper> getData() {
         return data;
     }
 }
